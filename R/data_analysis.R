a16bd6dbdd579732829b05744baa302be02711ab
yuriychen <ychen209@qq.com>
2021-05-20 11:18:47 +0800

    Initial commit

---
+++ R/data_analysis.R
@@ -0,0 +1,203 @@
+#' @title Calculate mean and sd value by conditions
+#' @description Calculate mean and sd value by conditions.
+#' @details Input dataframe from previous processing, then return a data set with mean and sd values by conditions.
+#' @param prot_dat A DataFrame from previous processing.
+#' @param meta A DataFrame of meta file.
+#' @return A DataFrame of mean and sd by conditions.
+#' @export
+data_calMean_calSD <- function(prot_dat, meta){
+  rownames(meta) <- meta$sample
+  condition_list <- unique(meta[colnames(prot_dat),'condition'])
+
+  prot_mean_sd <- as.numeric(rownames(prot_dat))
+  for (c in condition_list) {
+    sample_list <- meta$sample[meta$condition == c]
+    temp <- prot_dat[,sample_list]
+    mean_list <- apply(temp, 1, mean)
+    sd_list <- apply(temp, 1, sd)
+    mean_sd <- cbind(mean_list,sd_list)
+    mean_coln <- paste('mean_',c)
+    sd_coln <- paste('sd_',c)
+    colnames(mean_sd) <- c(mean_coln,sd_coln)
+    prot_mean_sd <- cbind(prot_mean_sd,mean_sd)
+  }
+  rownames(prot_mean_sd) <- prot_mean_sd[,1]
+  prot_mean_sd <- prot_mean_sd[,-1]
+
+  return(prot_mean_sd)
+}
+
+#' @title Draw volcano plot
+#' @description Draw volcano plot.
+#' @details Input dataframe from previous processing, then return a ggplot object of volcano plot.
+#' @param prot_dat_1 A DataFrame of condition 1.
+#' @param prot_dat_2 A DataFrame of condition 2.
+#' @param meta A DataFrame of meta file.
+#' @param df A integer of freedom dgree.
+#' @param xleft A numeric for x-axis limit on left.
+#' @param xright A numeric for x-axis limit on right.
+#' @param ydown A numeric for x-axis limit on bottom.
+#' @param ytop A numeric for x-axis limit on top.
+#' @param confidence A numeric of confidence interval.
+#' @param s0 A numeric of fudge factor s0.
+#' @param smooth_curve A boolean of significant curve or not.
+#' @return A list of dataframe and ggplot object.
+#' @export
+#' @import ggplot2
+#' @import ggrepel
+data_volcano <- function(prot_dat_1, prot_dat_2, meta, df, xleft=-5, xright=5, ydown=0, ytop=5, confidence=.95, s0=.1,smooth_curve=TRUE){
+  ta <- qt(confidence,df)
+
+  prot_dat_com <- cbind(prot_dat_1,prot_dat_2)
+  colnum1 <- ncol(prot_dat_1)
+  colnum2 <- ncol(prot_dat_2)
+  pvalue <- apply(prot_dat_com, 1, function(x){t.test(x[1:colnum1],x[(colnum1 + 1):(colnum2+colnum1)],paired = F)$p.value})
+  lgpvalue <- -log10(pvalue)
+
+  prot_mean_sd <- data_calMean_calSD(cbind(prot_dat_1,prot_dat_2),meta)
+  fc <- prot_mean_sd[,3] / prot_mean_sd[,1]
+  l2fc <- log2(fc)
+
+  prot_fudge <- data.frame(l2fc,lgpvalue)
+  colnames(prot_fudge) <- c('l2fc','lgpvalue')
+  rownames(prot_fudge) <- rownames(prot_dat_1)
+  prot_fudge$fudge <- apply(prot_fudge,1, function(x){data_calsmoothcurve(as.numeric(x[1]),ta,s0,df)})
+  prot_fudge$sig <- '0'
+  prot_fudge$sig <- apply(prot_fudge,1,function(x){ifelse(((as.numeric(x[2]) > as.numeric(x[3])) & ((as.numeric(x[1])) > ta*s0)),'1',x[4])})
+  prot_fudge$sig <- apply(prot_fudge,1,function(x){ifelse(((as.numeric(x[2]) > as.numeric(x[3])) & ((as.numeric(x[1])) < -ta*s0)),'-1',x[4])})
+
+  prot_fudge_sig <- subset(prot_fudge,sig=='1')
+
+  p <- ggplot(prot_fudge,aes(x=l2fc,y=lgpvalue,color=sig))+geom_point()+theme_bw()+
+    scale_color_manual(values = c('red','grey60','green'))+theme(legend.position = 'none',title = element_text(size=20),axis.text = element_text(size=16))+
+    xlab('Log2(Fold Change)')+ylab('-Log10(P-value)')+
+    geom_text_repel(prot_fudge_sig,mapping=aes(label=rownames(prot_fudge_sig)),vjust=1,size=5,color='red')+
+    xlim(xleft,xright)+ylim(ydown,ytop)
+
+  if (smooth_curve == TRUE){
+    p <- p + geom_function(fun=function(x){data_calsmoothcurve(x,ta,s0,df)},xlim=c(.02,xright),linetype='dashed',n=10000,size=2,alpha=.3,color='darkred')+
+      geom_function(fun=function(x){data_calsmoothcurve(x,ta,s0,df)},xlim=c(xleft,-.02),size=2,alpha=.3,color='darkgreen',n=10000,linetype='dashed')
+  }
+
+  return(list(prot_fudge,p))
+}
+
+#' @title Draw volcano plot with Labels
+#' @description Draw volcano plot with manual labels.
+#' @details Input dataframe from previous processing, then return a ggplot object of volcano plot.
+#' @param prot_dat_1 A DataFrame of condition 1.
+#' @param prot_dat_2 A DataFrame of condition 2.
+#' @param meta A DataFrame of meta file.
+#' @param df A integer of freedom dgree.
+#' @param xleft A numeric for x-axis limit on left.
+#' @param xright A numeric for x-axis limit on right.
+#' @param ydown A numeric for x-axis limit on bottom.
+#' @param ytop A numeric for x-axis limit on top.
+#' @param confidence A numeric of confidence interval.
+#' @param s0 A numeric of fudge factor s0.
+#' @param smooth_curve A boolean of significant curve or not.
+#' @param label A boolean of label or not.
+#' @param label_list A list of labels.
+#' @return A list of dataframe and ggplot object.
+#' @export
+#' @import ggplot2
+#' @import ggrepel
+data_volcano_label <- function(prot_dat_1, prot_dat_2, meta, df, xleft=-5, xright=5, ydown=0, ytop=5, confidence=.95, s0=.1,smooth_curve=TRUE,label=TRUE,label_list = 'none'){
+  ta <- qt(confidence,df)
+
+  prot_dat_com <- cbind(prot_dat_1,prot_dat_2)
+  colnum1 <- ncol(prot_dat_1)
+  colnum2 <- ncol(prot_dat_2)
+  pvalue <- apply(prot_dat_com, 1, function(x){t.test(x[1:colnum1],x[(colnum1 + 1):(colnum2+colnum1)],paired = F)$p.value})
+  lgpvalue <- -log10(pvalue)
+
+  prot_mean_sd <- data_calMean_calSD(cbind(prot_dat_1,prot_dat_2),meta)
+  fc <- prot_mean_sd[,3] / prot_mean_sd[,1]
+  l2fc <- log10(fc)
+
+  prot_fudge <- data.frame(l2fc,lgpvalue)
+  colnames(prot_fudge) <- c('l2fc','lgpvalue')
+  rownames(prot_fudge) <- rownames(prot_dat_1)
+  prot_fudge$fudge <- apply(prot_fudge,1, function(x){data_calsmoothcurve(as.numeric(x[1]),ta,s0,df)})
+  prot_fudge$sig <- '0'
+  prot_fudge$sig <- apply(prot_fudge,1,function(x){ifelse(((as.numeric(x[2]) > as.numeric(x[3])) & ((as.numeric(x[1])) > ta*s0)),'1',x[4])})
+  prot_fudge$sig <- apply(prot_fudge,1,function(x){ifelse(((as.numeric(x[2]) > as.numeric(x[3])) & ((as.numeric(x[1])) < -ta*s0)),'-1',x[4])})
+
+  prot_fudge_sig <- subset(prot_fudge,sig=='1')
+
+  prot_fudge$label <- ifelse(rownames(prot_fudge) %in% label_list,'10','0')
+  prot_fudge_label <- subset(prot_fudge,label=='10')
+
+  prot_fudge$symbol <- as.character(as.numeric(prot_fudge$sig) + as.numeric(prot_fudge$label))
+
+  if (label==TRUE){
+    p <- ggplot(prot_fudge,aes(x=l2fc,y=lgpvalue,color=symbol))+geom_point(size=3,shape=1,stroke=1.5)+theme_bw()+
+      scale_color_manual(values = c('red','grey60','green','blue','blue'))+
+      theme(legend.position = 'none',title = element_text(size=20),axis.text = element_text(size=16))+
+      xlab('Log2(Fold Change)')+ylab('-Log10(P-value)')+
+      geom_text_repel(prot_fudge_label,mapping=aes(label=rownames(prot_fudge_label)),vjust=-0.5,hjust=-0.5,size=5,color='blue')+
+      xlim(xleft,xright)+ylim(ydown,ytop)
+
+    if (smooth_curve == TRUE){
+      p <- p + geom_function(fun=function(x){data_calsmoothcurve(x,ta,s0,df)},xlim=c(.02,xright),linetype='dashed',n=10000,size=2,alpha=.3,color='darkred')+
+        geom_function(fun=function(x){data_calsmoothcurve(x,ta,s0,df)},xlim=c(xleft,-.02),size=2,alpha=.3,color='darkgreen',n=10000,linetype='dashed')
+    }
+
+  }
+  else {
+    p <- ggplot(prot_fudge,aes(x=l2fc,y=lgpvalue,color=symbol))+geom_point(size=3,shape=1,stroke=1.5)+theme_bw()+
+      scale_color_manual(values = c('red','grey60','green'))+theme(legend.position = 'none',title = element_text(size=20),axis.text = element_text(size=16))+
+      xlab('Log10(Fold Change)')+ylab('-Log10(P-value)')+
+      xlim(xleft,xright)+ylim(ydown,ytop)
+
+    if (smooth_curve == TRUE){
+      p <- p + geom_function(fun=function(x){data_calsmoothcurve(x,ta,s0,df)},xlim=c(.02,xright),linetype='dashed',n=10000,size=2,alpha=.3,color='darkred')+
+        geom_function(fun=function(x){data_calsmoothcurve(x,ta,s0,df)},xlim=c(xleft,-.02),size=2,alpha=.3,color='darkgreen',n=10000,linetype='dashed')
+    }
+  }
+
+  return(list(prot_fudge,p))
+}
+
+data_calsmoothcurve = function(x,ta,s0,df){
+  if (x > 0) {
+    y = x * ta / (x - (ta * s0))
+    y = -log10(2*(1-pt(y,df=df)))
+    return(y)
+  }
+  else if (x < 0) {
+    y = x * ta / (x + (ta * s0))
+    y = -log10(2*(1-pt(y,df=df)))
+    return(y)
+  }
+}
+
+#' @title Draw Specific Protein Intensity plot
+#' @description Draw Specific Protein Intensity plot.
+#' @details Input dataframe from data_transform_melt, then return a ggplot object of intensity plot.
+#' @param prot_dat_specific A DataFrame from data_transform_melt.
+#' @param id A integer or string.
+#' @return A ggplot object.
+#' @export
+#' @import ggplot2
+data_draw_specific <- function(prot_dat_specific, id){
+  p <- ggplot(prot_dat_specific[prot_dat_specific[,1]==id,],aes(x=condition,y=value))+geom_point(size=2)+
+    theme_bw()+theme(axis.title = element_text(size=20),axis.text = element_text(size=18,angle = 60,hjust = 1),title = element_text(size=22))+
+    xlab('')+ylab('Relative Intensity')+ggtitle(id)
+  return(p)
+}
+
+#' @title Transform DataFrame by melt
+#' @description Transform DataFrame by melt.
+#' @details Input dataframe from previous processing, then return a DataFame of conditions melt.
+#' @param prot_dat A DataFrame from previous processing.
+#' @param meta A DataFrame of meta file.
+#' @return A DataFrame of melt.
+#' @export
+#' @import reshape2
+data_transform_melt <- function(prot_dat, meta){
+  prot_dat <- cbind(rownames(prot_dat),prot_dat)
+  prot_dat_specific <- melt(prot_dat)
+  prot_dat_specific$condition <- apply(prot_dat_specific,1,function(x){meta$condition[(meta$sample == x[2])]})
+  return(prot_dat_specific)
+}
---
+++ R/data_read.R
@@ -0,0 +1,204 @@
+#' @title Read, clean and annotate MS TMT data.
+#' @description Read, clean and annotate MS TMT data with Maxquant process data and meta file.
+#' @details Input MaxQuang processed MS TMT data and meta file, then return a MSDataSet object.
+#' @param prot_raw A DataFrame of proteinGroups from MaxQuant.
+#' @param meta A DataFrame of metafile.
+#' @param name A character of the name.
+#' @param reference A boolean.
+#' @param analysis_type A character.
+#' @param exclusion_status A character.
+#' @param normalization_status A vector.
+#' @import edgeR
+#' @import RColorBrewer
+#' @import psych
+#' @import ggplot2
+#' @export
+read_maxquant_prot <- function(prot_raw,meta,name='defalut',reference=FALSE,analysis_type='TMT',exclusion_status='none',normalization_status=c('none')){
+  prot_dat_clean <- data_clean(prot_raw)
+  prot_dat_extract <- data_extract(prot_dat_clean,meta)
+  prot_annotation <- data_annotation(prot_dat_extract)
+
+  MSDataSet <- setRefClass('MSDataSet',
+    fields = list(
+      name='character',
+      analysis_type='character',
+      exclusion_status='character',
+      zero_substitution_status='character',
+      normalization_status='vector',
+      reference='logical',
+      meta='data.frame',
+      dataset='data.frame',
+      dataset_raw='data.frame',
+      dataset_annotation='data.frame',
+      dataset_count='data.frame',
+      dataset_count_excluded='data.frame',
+      samples='vector',
+      conditions='vector',
+      repeats='vector',
+      reference_samples='vector',
+      num_sample='numeric',
+      num_condition='numeric',
+      num_repeat='numeric',
+      mean_condition='data.frame',
+      sd_condition='data.frame'
+    ),
+    methods = list(
+      summary = function(){
+        cat('name: ',name,'\n')
+        cat('analysis_type: ',analysis_type,'\n')
+        cat('exclusion_status: ',exclusion_status,'\n')
+        cat('zero_substitution_status: ',zero_substitution_status,'\n')
+        cat('normalization_status: ',normalization_status,'\n')
+        cat('samples: ',samples,'\n')
+        cat('conditions: ',conditions,'\n')
+        cat('reference_samples: ',reference_samples,'\n')
+        cat('num_sample: ',num_sample,'\n')
+        cat('num_condition: ',num_condition,'\n')
+        cat('num_repeat: ',num_repeat,'\n')
+        cat('dataset: ',str(dataset),'\n')
+        cat('dataset annotation: ',str(dataset_annotation),'\n')
+      },
+      exclude = function(zerotolinrep=0,zerotolamongreps=0){
+        meta_noref <- meta[is.na(meta$reference),]
+        datacount <- dataset
+        datacount[datacount > 0] <- 1
+        for (r in repeats){
+          datacount[,as.character(r)] <- rowSums(datacount[,meta[meta$repeat. == r,'sample']])
+          datacount[(datacount[,as.character(r)] < (num_condition - zerotolinrep)),as.character(r)] <- 0
+          datacount[(datacount[,as.character(r)] >= (num_condition - zerotolinrep)),as.character(r)] <- 1
+        }
+        datacount[,'total'] <- rowSums(datacount[,as.character(repeats)])
+        datacount[(datacount[,'total'] < (num_repeat - zerotolamongreps)),'total'] <- 0
+        datacount[(datacount[,'total'] >= (num_repeat - zerotolamongreps)),'total'] <- 1
+        dataset <<- dataset[datacount[,'total'] == 1,]
+        dataset_annotation <<- dataset_annotation[datacount[,'total'] == 1,]
+        dataset_count <<- datacount
+        dataset_count_excluded <<- datacount[datacount[,'total'] == 1,]
+        exclusion_status <<- paste('Exluded by',zerotolinrep,'zero tol in rep and',zerotolamongreps,'zero tol among rep.')
+      },
+      normalization_sl = function(){
+        target <- mean(colSums(dataset))
+        norm_facs <- target / colSums(dataset)
+        prot_dat_sl <- sweep(dataset,2,norm_facs,FUN = '*')
+        dataset <<- prot_dat_sl
+        normalization_status <<- append(normalization_status,'SL')
+      },
+      normalization_tmm = function(){
+        #need edgeR
+        target <- calcNormFactors(dataset)
+        prot_dat_tmm <- sweep(dataset,2,target,FUN='/')
+        dataset <<- prot_dat_tmm
+        normalization_status <<- append(normalization_status,'TMM')
+      },
+      normalization_irs = function(){
+        i <- 1
+        irs <- cbind(as.numeric(rownames(dataset)),rowSums(dataset[,1:num_condition]))
+        while (i < num_repeat){
+          start_site <- i * num_condition + 1
+          stop_site <- i * num_condition + num_condition
+          irs <- cbind(irs, rowSums(dataset[,start_site:stop_site]))
+          i <- i + 1
+        }
+        irs <- irs[,-1]
+        irs$average <- apply(irs,1,function(x){exp(mean(log((x))))})
+        
+        i <- 1
+        prot_dat_irs <- cbind(rownames(dataset),(dataset[,1:num_condition] * irs$average / rowSums(dataset[,1:num_condition])))
+        while (i < num_repeat){
+          start_site <- i * num_condition + 1
+          stop_site <- i * num_condition + num_condition
+          prot_dat_irs <- cbind(prot_dat_irs,(dataset[,start_site:stop_site] * irs$average / rowSums(dataset[,start_site:stop_site])))
+          i <- i + 1
+        }
+        
+        rownames(prot_dat_irs) <- prot_dat_irs[,1]
+        prot_dat_irs <- prot_dat_irs[,-1]
+        
+        dataset <<- prot_dat_irs
+        normalization_status <<- append(normalization_status,'IRS')
+      },
+      zero_substitution = function(least=30){
+        for (r in repeats) {
+          temp <- (dataset_count_excluded[,as.character(r)] == 1)
+          dataset[dataset[temp,] == 0] <<- least
+          zero_substitution_status <<- paste('zero values were substituted by',least)
+        }
+      },
+      graphic_draw = function(state=''){
+        #need RColorBrewer
+        qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+        col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+        
+        par(mfrow = c(2, 2))
+        boxplot(log2(dataset),col = rep(col_vector[9:(num_repeat+8)],each=num_condition), main = state)
+        plotDensities(log2(dataset),col = rep(col_vector[9:(num_repeat+8)],num_condition), main = state)
+        plotMDS(log2(dataset), col = col_vector[9:(num_condition+8)], main = state)
+        data_calCV_draw(dataset,num_condition,num_repeat, state)
+        par(mfrow = c(1, 1)) # reset to default
+      },
+      pearson_draw = function(state='',lm=TRUE){
+        prot_dat_reorder <- data_rearrange(dataset,num_condition,num_repeat)
+        i <- 1
+        #need psych
+        pairs.panels(log2(prot_dat_reorder[,1:num_repeat]), lm = lm,main=state)
+        while (i < num_condition) {
+          start_site <- i * num_repeat + 1
+          stop_site <- i * num_repeat + num_repeat
+          pairs.panels(log2(prot_dat_reorder[,start_site:stop_site]), lm = lm,main=state)
+          i <- i + 1
+        }
+      },
+      proteins_summary = function(){
+        temp <- dataset_raw[,meta$sample]
+        colnames(temp) <- meta$sample
+        temp_count <- temp
+        temp_count[temp_count > 0] <- 1
+        p_count <- cbind(colnames(temp_count),colSums(temp_count))
+        p_count <- data.frame(p_count)
+        p_count[,1] <- factor(p_count[,1],levels=p_count[,1])
+        p_count[,2] <- as.numeric(p_count[,2])
+        p_c <- ggplot(data=p_count,aes(x=p_count[,1],y=p_count[,2]))+geom_bar(stat='identity') +
+          theme_bw() + theme(axis.text.x = element_text(angle = 60,hjust=1),legend.position = 'none',axis.text = element_text(size=12))+
+          xlab('')+ylab('')+ggtitle('Protein Count')
+        
+        temp_inten <- temp
+        p_inten <- cbind(colnames(temp_inten),colSums(temp_inten))
+        p_inten <- data.frame(p_inten)
+        p_inten[,1] <- factor(p_inten[,1],levels=p_inten[,1])
+        p_inten[,2] <- as.numeric(p_inten[,2])
+        p_i <- ggplot(data=p_count,aes(x=p_inten[,1],y=p_inten[,2]))+geom_bar(stat='identity') +
+          theme_bw() + theme(axis.text.x = element_text(angle = 60,hjust=1),legend.position = 'none',axis.text = element_text(size=12))+
+          xlab('')+ylab('')+ggtitle('Protein Total Intensity')
+        
+        return(list(p_c,p_i))
+      }
+    )
+    )
+
+  msdataset <- MSDataSet$new(
+    name=name,
+    analysis_type=analysis_type,
+    exclusion_status=exclusion_status,
+    zero_substitution_status='none',
+    normalization_status=normalization_status,
+    reference=reference,
+    meta=meta,
+    dataset=prot_annotation[[1]],
+    dataset_raw=prot_annotation[[1]],
+    dataset_annotation=prot_annotation[[2]],
+    dataset_count=data.frame(),
+    dataset_count_excluded=data.frame(),
+    samples=meta$sample[is.na(meta$reference)],
+    conditions=unique(meta$condition[is.na(meta$reference)]),
+    repeats=unique(meta[,'repeat.']),
+    reference_samples= ifelse(reference,meta$sample[meta$reference==1],'none'),
+    num_sample=nrow(meta[is.na(meta$reference),]),
+    num_condition=(nrow(meta[is.na(meta$reference),]) / max(meta[,'repeat.'])),
+    num_repeat=length(unique(meta[,'repeat.'])),
+    mean_condition=data.frame(),
+    sd_condition=data.frame()
+  )
+
+  return(msdataset)
+}
+
